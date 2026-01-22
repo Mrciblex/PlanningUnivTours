321dfee6429cafc0a130b197416879e2b9d0fdfe
FabienGuitton <fabienguittonfr@gmail.com>
2026-01-22 18:59:11 +0100

    testÃ

--- src/main/java/com/planning/info/InfoApplication.java
+++ src/main/java/com/planning/info/InfoApplication.java
@@ -8,6 +8,7 @@ public class InfoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InfoApplication.class, args);
+        System.out.println("Application lancé..");
     }
 
 }
