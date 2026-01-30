f278d9a7a206eb32af78f771d906abb1556a008a
Mrciblex <musa.37400@gmail.com>
2026-01-30 17:22:11 +0100

    Debut algo + ecrits

--- src/main/java/com/univtime/informatique/UnivTime.java
+++ src/main/java/com/univtime/informatique/UnivTime.java
@@ -3,11 +3,30 @@ package com.univtime.informatique;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class UnivTime {
     public static void main(String[] args) {
-        SpringApplication.run(UnivTime.class, args);
+        //SpringApplication.run(UnivTime.class, args);
         System.out.println("Application lancé..");
-        System.out.println("test");
+        System.out.println("----------------- TEST ALGO -----------------");
+
+
     }
+
+    record MomentBanalise (
+            String nom,
+            LocalDateTime debut,
+            LocalDateTime fin
+    ) {}
+
+    record Semestre(
+            LocalDate debut,
+            LocalDate fin
+    ) {}
+
+
+
 }
