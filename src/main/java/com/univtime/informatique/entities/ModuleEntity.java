e0c8fe0dc0f0142f65f45704d007ca32dd40c12b
FabienGuitton <fabienguittonfr@gmail.com>
2026-01-31 14:14:31 +0100

    Mise à jour de l'architecture + Remplissage de ModuleEntity (Sans liens)

--- src/main/java/com/univtime/informatique/entities/ModuleEntity.java
+++ src/main/java/com/univtime/informatique/entities/ModuleEntity.java
@@ -1,4 +1,40 @@
 package com.univtime.informatique.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "modules")
 public class ModuleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idModule", nullable = false)
+    private Integer idModule;
+
+    @Column(name = "nomModule", nullable = false, length = 150)
+    private String nomModule;
+
+    public ModuleEntity() {
+
+    }
+
+    public ModuleEntity(Integer idModule, String nomModule) {
+        this.idModule = idModule;
+        this.nomModule = nomModule;
+    }
+
+    public Integer getIdModule() {
+        return idModule;
+    }
+
+    public void setIdModule(Integer idModule) {
+        this.idModule = idModule;
+    }
+
+    public String getNomModule() {
+        return nomModule;
+    }
+
+    public void setNomModule(String nomModule) {
+        this.nomModule = nomModule;
+    }
 }
