d2d75502c1b9e3ddcbf5f72dd08e1ef0059634dd
Mrciblex <musa.37400@gmail.com>
2026-02-01 05:05:39 +0100

    Codage des placements en cours...

--- src/main/java/com/univtime/informatique/UnivTime.java
+++ src/main/java/com/univtime/informatique/UnivTime.java
@@ -13,6 +13,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class UnivTime {
@@ -51,6 +52,7 @@ public class UnivTime {
          * Boucle sur tout les jours du semestre
          */
         long nbSemaine = ChronoUnit.WEEKS.between(firstWeek, lastWeek) + 1;
+        Map<Integer, Map<Integer, List<Slot>>> slotsStorage = new HashMap<>();
         for (int weekOffset = 0; weekOffset < nbSemaine; weekOffset++){
             // Début et fin de la semaine du calendrier
             LocalDate weekStart = firstWeek.plusWeeks(weekOffset);
@@ -65,13 +67,12 @@ public class UnivTime {
             if (weekEnd.isAfter(semestre.fin())){
                 weekEnd = semestre.fin();
             }
-
+            Map<Integer, List<Slot>> daySlots = new HashMap<>();
             for (int dayOffset = weekStart.getDayOfWeek().getValue() - 1; dayOffset < weekEnd.getDayOfWeek().getValue(); dayOffset++){
                 LocalDate actualDay = semestre.debut().plusWeeks(weekOffset).plusDays(dayOffset);
                 boolean isExclude = isExcludeDay(actualDay, excludedDays);
 
                 if (!isExclude){
-                    System.out.println(actualDay + " :");
                     /**
                      * Création des slots de la semaine
                      */
@@ -79,24 +80,56 @@ public class UnivTime {
                     for(PlanningPeriodMinutes period : planningPossiblePeriod){
                         for (int start = period.debut(); start < period.fin(); start = start + slotStep){
                             boolean isBlocked = false;
-                            Slot slot = new Slot(start, start + slotStep, isBlocked)
+                            Slot slot = new Slot(start, start + slotStep, isBlocked);
                             slotOfDay.add(slot);
                         }
                     }
-                    slotOfDay.forEach(slot -> {
-                        // AFFICHAGE A FAIRE
-                    });
+                    daySlots.put(dayOffset + 1, slotOfDay);
                 }
             }
+            slotsStorage.put(weekOffset + 1, daySlots);
+
+            // ------------ Placement des cours dans les slots ------------
+            // On a tous les slots de la semaine actuel qui viennent d'être créés
+            // Il faut boucler avec les profs (ordered) puis les cours à placer (ordered)
+            // Boucler sur les slots de la semaine qui sont possible (verif prof et groupe)
+            // Vérifier si le slot est pas dans un moment banalisé aussi, puis rank tous les placements de cours possible
+            // Choisir aléatoire parmi x pourcent des meilleurs un placement
+            // Une fois les slots de la semaine remplis, on améliore l'organisation
+            // Il faut pour chaque slots essayer de replacer ses cours autre part dans la semaine et voir si le score de la semaine augmente
+            // (Si un slot ou le score augmente est déjà pris, alors on essaie de déplacer celui là aussi mais on dit que le slot est désormais figé
+            // C'est à dire que la prochaine itération d'amélioration de week, ne pourra pas changer ce slot
+            // L'amélioration devrait être ordered avec les Slots allant de score le moins bon au meilleur (pour éviter de bouger les bons scores)
+
+            // PB : Dans ce cas de relocalisation, si on remplace un slot occupé :
+            // Est ce que c'est possible de re-placer le second slot (disponibilités) ? Oui, Est ce que c'est un meilleur score de semaine ? Oui
+            // Alors on remplace le slot (ou les slots si plusieurs ont dû bouger) et on le(s) fige
+            // Timeout si impossible ou score de semaine moins bien, ce après quoi : re-tentative d'up le score grâce au mouvement du slot (autre part parmi les meilleurs choix) x fois
+            // Si aucune amélioration possible pour ce slot (score week inférieur ou impossibles disponibilités) ou score de nouveau placement inférieur,
+            // Alors on ne fige que le slot qu'on peut pas améliorer, le rest des lots testés ne doivent pas être figé si impossible ou score week inférieur.
+
+            // Dans le cas ou le slot nouveau meilleur slot est libre, alors on déplace et on fige le nouveau slot
+
+            // Le calcul du Weight doit être optimisé car il sera souvent appelé pour tester l'amélioration du planning !
+            // Les poids se calcul via la semaine
+
         }
 
 
+
+        // DEBUG
+        slotsStorage.forEach((week, daySlots) -> {
+            System.out.print("Semaine " + week);
+            daySlots.forEach((day, slot) -> {
+                System.out.print(" Jour " + day + " : \n");
+                System.out.println(slot);
+            });
+        });
     }
 
     static boolean isDateAvailable(
             LocalDateTime jour,
             List<MomentBanalise> momentBanalises){
-
         return false;
     }
 
@@ -179,8 +212,13 @@ public class UnivTime {
         @Override
         public String toString() {
             // Re-écrire cette méthode
-            // Début de solution : System.out.println("Slot [" + (int) (slot.getDebut() / 60) + ":" + (int) (slot.getFin() % 60) + "]");
-            return super.toString();
+            return String.format(
+                    "Slot [%dh%02d - %dh%02d] isBlocked = " + isBlocked + " | \n" + "---Cours : " + usedBy + "\n",
+                    (int) (this.getDebut() / 60),
+                    (int) (this.getDebut() % 60),
+                    (int) (this.getFin() / 60),
+                    (int) (this.getFin() % 60)
+            );
         }
     }
 
