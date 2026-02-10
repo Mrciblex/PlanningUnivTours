cb2c1d36960ce5c6fc478bda44e66c267ff03459
FabienGuitton <fabienguittonfr@gmail.com>
2026-02-10 21:37:57 +0100

    Changement des parametres JpaRepository

--- src/main/java/com/univtime/informatique/repositories/ParticipeARepository.java
+++ src/main/java/com/univtime/informatique/repositories/ParticipeARepository.java
@@ -1,8 +1,8 @@
 package com.univtime.informatique.repositories;
 
-import com.univtime.informatique.entities.DisponibiliteEntity;
+import com.univtime.informatique.entities.ParticipeAEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParticipeARepository extends JpaRepository<DisponibiliteEntity, Long> {
+public interface ParticipeARepository extends JpaRepository<ParticipeAEntity, Integer> {
 
 }
--- src/main/java/com/univtime/informatique/repositories/PromoEstComposeeRepository.java
+++ src/main/java/com/univtime/informatique/repositories/PromoEstComposeeRepository.java
@@ -3,6 +3,6 @@ package com.univtime.informatique.repositories;
 import com.univtime.informatique.entities.PromoEstComposeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PromoEstComposeeRepository extends JpaRepository<PromoEstComposeeEntity, Long> {
+public interface PromoEstComposeeRepository extends JpaRepository<PromoEstComposeeEntity, Integer> {
 
 }
