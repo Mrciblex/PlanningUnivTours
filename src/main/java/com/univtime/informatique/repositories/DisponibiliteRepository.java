8307745140bc73a96374c1f922c46daa54be342b
FabienGuitton <fabienguittonfr@gmail.com>
2026-02-10 20:53:53 +0100

    IChangement des paramètres de JpaRepository de Jour et Disponibilite

--- src/main/java/com/univtime/informatique/repositories/DisponibiliteRepository.java
+++ src/main/java/com/univtime/informatique/repositories/DisponibiliteRepository.java
@@ -3,6 +3,6 @@ package com.univtime.informatique.repositories;
 import com.univtime.informatique.entities.DisponibiliteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DisponibiliteRepository extends JpaRepository<DisponibiliteEntity, Long> {
+public interface DisponibiliteRepository extends JpaRepository<DisponibiliteEntity, Integer> {
 
 }
--- src/main/java/com/univtime/informatique/repositories/JourRepository.java
+++ src/main/java/com/univtime/informatique/repositories/JourRepository.java
@@ -1,8 +1,8 @@
 package com.univtime.informatique.repositories;
 
-import com.univtime.informatique.entities.DisponibiliteEntity;
+import com.univtime.informatique.entities.JourEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JourRepository extends JpaRepository<DisponibiliteEntity, Long> {
+public interface JourRepository extends JpaRepository<JourEntity, Integer> {
 
 }
