a878f3348a91aa40cc38eb237c3081bb2eac622d
flags32 <drop.animer@gmail.com>
2026-02-19 23:15:12 +0100

    explication

--- src/main/java/com/univtime/informatique/controllers/GestionnaireEdtController.java
+++ src/main/java/com/univtime/informatique/controllers/GestionnaireEdtController.java
@@ -5,22 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/gestionnaire-edt")
 public class GestionnaireEdtController {
-
+    // pour la vue edt
     @GetMapping("/edt")
     public String edt() {
         return "gestionnaire_edt/edt";
     }
-
+    // pour la vue composantes
     @GetMapping("/composantes")
     public String composantes() {
         return "gestionnaire_edt/gestion_composantes";
     }
-
+    // pour la vue groupe
     @GetMapping("/groupes")
     public String groupes() {
         return "gestionnaire_edt/gestion_groupes";
     }
-
+    // proffesseurs
     @GetMapping("/professeurs")
     public String professeurs() {
         return "gestionnaire_edt/gestion_professeurs";
