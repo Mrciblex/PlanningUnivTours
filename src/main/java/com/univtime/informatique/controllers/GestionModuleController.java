5707d5e06eb56db86c27f43246890142e49c3f88
Léo <ronflax1@gmail.com>
2026-03-01 16:53:13 +0100

    ajout methodes GestionModuleController

--- src/main/java/com/univtime/informatique/controllers/GestionModuleController.java
+++ src/main/java/com/univtime/informatique/controllers/GestionModuleController.java
@@ -4,9 +4,7 @@ import com.univtime.informatique.dto.moduleDto.ModuleDto;
 import com.univtime.informatique.services.ModuleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +32,36 @@ public class GestionModuleController {
         model.addAttribute("idPromo", idPromo);
 
         return "gestionnaire_edt/gestion_modules";
+    }
+
+    /**
+     * URL : /gestionnaire-edt/modules/{id}
+     */
+    @GetMapping("/{id}")
+    public String getModuleById(@PathVariable Integer id, Model model) {
+        ModuleDto module = moduleService.findModuleDtoById(id);
+        model.addAttribute("module", module);
+        return "gestionnaire_edt/moduleDetail";
+    }
+
+    @PostMapping("/new")
+    public String createModule(@ModelAttribute ModuleDto moduleDto) {
+        ModuleDto saved = moduleService.createModule(moduleDto);
+        return "redirect:/gestionnaire-edt/modules/" + saved.getIdModule();
+    }
+
+    @PostMapping("/{id}/edit")
+    public String updateModule(@PathVariable Integer id, @ModelAttribute ModuleDto moduleDto) {
+
+        moduleDto.setIdModule(id);
+        moduleService.updateModule(moduleDto);
+
+        return "redirect:/gestionnaire-edt/modules/" + id;
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteModule(@PathVariable Integer id) {
+        moduleService.deleteModuleById(id);
+        return "redirect:/gestionnaire-edt/modules";
     }
 }
